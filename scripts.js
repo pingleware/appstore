7e4f2f1318ed23095f8dc7919b631dd3e9d474d0
PIPNGLEWARE <maintainers@pingleware.work>
2025-04-30 11:51:07 -0400

    modife app.json to include multiple download links

--- scripts.js
+++ scripts.js
@@ -9,13 +9,17 @@ apps.forEach(app => {
   fetch(`apps/${app}/info.json`)
     .then(res => res.json())
     .then(data => {
+      let download_links = "";
       const card = document.createElement("div");
+      data.download.forEach(download => {
+        console.log(download);
+      });
       card.className = "app-card";
       card.innerHTML = `
         <img src="${data.image}" alt="${data.name}" width="100" />
         <h2>${data.name}</h2>
         <p>${data.description}</p>
-        <a href="${data.download}" target="_blank">Download</a>
+        <p>Download: ${download_links}</p>
       `;
       appList.appendChild(card);
     });
